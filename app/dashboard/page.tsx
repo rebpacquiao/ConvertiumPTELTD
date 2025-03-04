54fed82b5d4ebb40a44e11f6054a66fabc300dc3
rebpacquiao <reb.pacquiao@gmail.com>
2025-03-04 16:45:43 +0800

    Remove edit links from PersonalPreferences, PersonalInformation, AdditionalDetails, and SpouseDetails components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,6 @@ export default function Dashboard() {
   return (
     <>
       <div className="flex h-screen bg-gray-200">
-        <Sidebar />
         <div className="flex flex-col flex-1 overflow-hidden">
           <AppBarMenu />
           <DashboardContent />
